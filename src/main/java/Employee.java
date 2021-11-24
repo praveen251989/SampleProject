c3de1572b43ac9f484181513c11f0ca3a322de65
Praveen Penumatsa <Praveen.Penumatsa@ifast-talk.com>
2021-11-24 12:40:47 -0500

    Initial commit

---
+++ src/main/java/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+    public int some_constant;
+
+    public int sum(int a ,int b){
+        return (a+b);
+    }
+    public static void main(String[] args) {
+        Employee e  = new Employee();
+        int val = e.sum(3,4);
+        System.out.println("This is a sample project");
+    }
+}
